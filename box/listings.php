7278dfd6271c228f3a9bb06015f067642b023bc5
richmilton <richard@bodymatter.com>
2013-08-08 07:50:49 +0100

    fix overlay

--- box/listings.php
+++ box/listings.php
@@ -147,7 +147,7 @@
 			</div>
 			<div class="overlay">
 				<div class="page">
-					<article class="film">
+					<!--article class="film">
 						<dl>
 							<dt>Those Magnificent Men in Their Flying Machines <span class="year">(1965)</span></dt>
 							<dd class="p-time"><span class="hour">9</span>:<span class="minutes">00</span> &ndash; <span class="hour">10</span>:<span class="minutes">30</span></dd>
@@ -168,11 +168,34 @@
 							<figcaption>Philip Larkin had it right...</figcaption>
 						</figure>
 						<div class="col">
-							<p>A rare event for cult movie fans, BBC Four, that doyen of sophisticated European TV and film (yeah, right) brings this cinematic classic to the viewers of post-watershed crap TV.</p>
+							<p>A rare event for cult movie fans, BBC Four, that doyen of sophiscated European TV and film (yeah, right) brings this cinematic classic to the viewers of post-watershed crap TV.</p>
 							<p>Hey, I could write TV reviews for a living. A few superlatives here, a few choice words there. Easy. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
 							<p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
 						</div>
-					</article>
+					</article-->
+          <article class="film">
+							<dl style="width:90%">
+								<dt id="p-name"></dt>
+								<dd id="p-time"></dd>
+								<dd id="p-channel"></dd>
+								<dd id="p-details">
+									<ul>
+										<!--li class="hd">HD</li>
+										<li class="widescreen">W</li>
+										<li class="subtitles">S</li-->
+										<li class="repeat"></li>
+										<li class="replay"></li>
+										<li class="length"></li>
+									</ul>
+								</dd>
+							</dl>
+							<figure>
+								<img id="p-img" src="" alt="" />
+								<figcaption></figcaption>
+							</figure>
+							<div id="p-desc" class="col"></div>
+								
+						</article>
 					<a href="" class="closeOverlay">Close</a>
 				</div>
 			</div>
