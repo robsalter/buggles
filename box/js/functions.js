54c991b6ac2c61fbbc383cd5b394ee6e1c76fd5f
richmilton <richard@bodymatter.com>
2013-08-08 07:52:12 +0100

    fix overlay

--- box/js/functions.js
+++ box/js/functions.js
@@ -1,248 +1,219 @@
-$(document).ready(function () {
-    loadAll();
-});
-
-var loaded = 0; day = 'today', now = new Date(), activeChannel = 1, cache = {}, uris = {
-    bbc1: {uri:'bbcone', regions: ['london'], color:'#df180e'}, 
-    bbc2: {uri:'bbctwo', regions: ['england','wales','scotland','ni','hd'], color:'#01454e'},
-    bbc3: {uri:'bbcthree', regions: [], color:'#450e38'},
-    bbc4: {uri:'bbcfour', regions: [], color:'#000000'}/*
-,
-    cbbc: {uri:'cbbc', regions: [], color:'#ff0000'},
-    cbeebies: {uri:'cbeebies', regions: [], color:'#ff0000'},
-    parliament: {uri:'bbc_parliament', regions: [], color:'#ff0000'},
-    news: {uri:'bbcnews', regions: [], color:'#ff0000'}
-*/
-};
-
-var loadAll = function () {
-    var uri;
-    for (uri in uris) {
-        getList(uri, true);
-    }
-}
-
-var getList = function (channel, hide) {
-  var h = '', dt, uri = uris[channel].uri, reg = uris[channel].regions[0] || '';
-  if (!cache[channel]) {
-    $.ajax({url:'services/l.php', data:{uri:uri, day:day, region:reg}, type:'POST'}).done(function(d) {
-      cb(d, channel, hide);
-      cache[channel] = d;
-      checkDone();
-    });
-  }
-  else {
-    cb(cache[channel], channel, hide);
-  }
-}
-
-var checkDone = function() {
-    loaded++;
-    if (loaded === 4) {
-        init();
-    }
-}
-
-var cb = function (d, c, hide) {
-  var h = '', x, y, ipLnk, dt, s, i;
-  for (x in d) {
-    y = d[x];
-    s = y.duration/300;
-    ipLnk = y.programme.is_available_mediaset_pc_sd ? '<a href="http://www.bbc.co.uk/iplayer/episode/' + y.programme.pid +'">Catch-up</a>' : '';
-    dt = new DateObj(y.start);
-    h += '<div class="m-group"><article class="' + c + '-' + x +'"><dl><dt>' + y.programme.display_titles.title + '</dt>';
-    h += '<dd class="p-time"><span class="hour">' + dt.hh + '</span>:<span class="minutes">' + dt.mm + '</span></dd></dl>';
-    h += '<ul class="extras"><ul><li class="replay">' + ipLnk + '</li></ul></article></div>';
-    for (i = 1; i <= s; i++) {
-      h += '<div class="m-group"></div>';
-    }
-  }
-  if (!cache[c]) {
-    $('#' + c).append(h);
-  }
-}
-
-DateObj = function (s) {
-  var bits  = s.split('T'),
-  date = bits[0].split('-');
-  time = bits[1].split(':');
-  
-  this.yyyy = date[0];
-  this.MM = date[1];
-  this.dd = date[2];
-  this.hh = parseInt(time[0]);
-  this.ampm = this.hh >=12 ? 'pm' : 'am';
-  this.mm = time[1];
-  this.str = this.dd + ' ' + this.MM + ' ' + this.yyyy + ' ' + this.hh + ' ' + this.mm;
-  this.t = (this.hh >=12 ? this.hh - 12 : this.hh) + ':' + this.mm + this.ampm;
-  //this.tense = function(){alert('hello')};
-}
-
-var init = function() {
-	// Appends classes for activing CSS transitions
-	// Should improve performance
-	$('.exposeBack').click(function(e){
-		$('.front').toggleClass('backExposed');
-		$('.wrapper > header').toggleClass('headerMoved');
-		e.preventDefault();
-	});
-	// Expose overlay
-	$('article').click(function(e){
-    	var cl = $(this).attr('class').split('-'), o = cache[cl[0]][cl[1]];
-		e.preventDefault();
-		console.log(o);
-		$('.overlay').addClass('overlayExposed');
-	});
-	$('.closeOverlay').click(function(e){
-		$('.overlay').removeClass('overlayExposed');
-		e.preventDefault();
-	});
-	$('.exposeCalendar').click(function(e){
-		$('.container').toggleClass('active');
-		$('.calendar').toggleClass('active');
-		//$('.exposeBack').parent().parent().toggleClass('calendarExposed');
-		// switch off exposeBack
-		e.preventDefault();
-	});
-
-	// Temporary measure until how to let user switch between columns is figured out
-	$('.exposeColumns').click(function(e){
-		$('.channels').css({
-	    	'width':'100%'
-    	});
-		e.preventDefault();
-	});
-
-    // Helps keep channel names in view, doesn't work so well on phones :(
-   
-    // seems to work quite well, although not when scrolling up
-    //$('.channel>h1').jScroll({speed : 200, top: 55});
-
-    // Performance depletion specialist codeage
-	$('.channels>ul').scroll(function (e) {
-    	var elementScrollTop = $('.channels>ul').scrollTop();
-    	$('.channel>h1').css({
-        	"top" : + elementScrollTop
-    	});
-    	e.preventDefault();
-    });
-    
-	// Change into 12/24 hour times + change theme   
-
-	$('.l-theme').click(function(e) {
-		$('body').removeClass('dark');
-		$(this).attr('disabled', true).siblings('button').attr('disabled', false);
-		e.preventDefault();
-	});
-	$('.d-theme').click(function(e) {
-		$('body').addClass('dark');
-		$(this).attr('disabled', true).siblings('button').attr('disabled', false);
-		e.preventDefault();
-	});
-	
-	//$('.sd-time').hide();
-	$('.sd-time').click(function(e) {
-		$('.hour').each(function() {
-			var currentValue = $(this).text();
-			var newValue = parseInt(currentValue) - 12;
-			$(this).text(newValue);
-		});
-		$(this).attr('disabled', true).siblings('button').attr('disabled', false);
-		e.preventDefault();
-	});
-	// Doesn't work for AM times (they'll get changed as well!)
-	$('.dd-time').click(function(e) {
-		$('.hour').each(function() {
-			var currentValue = $(this).text();
-			var newValue = parseInt(currentValue) + 12;
-			$(this).text(newValue);
-		});
-		$(this).attr('disabled', true).siblings('button').attr('disabled', false);
-		e.preventDefault();
-	});
-	
-	// Probably not optimised (and overly complicated), but I wrote some JS and it flippin' worked!!!
-	// Target channel lists (but per row, rather than using global content to inform all rows + cols)
-
-	$('.channel .gutter').each(function() {
-		// Loop for each hour of day
-		for (var i = 1; i <= 288; i++) {
-			// Target only direct children, load into variable
-			var nth = $(this).find('> :nth-child(' + [i] +')')
-			// Use looping variable to add hourly class name
-			$(nth).addClass('m' + '-' + [i]);
-			// Var up max height
-			var maxHeight = -1;
-			// Use looping number to fix heights in each hour row based on its longest piece of content
-			$('.m-' + [i]).each(function() {
-				maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
-			});
-			$('.m-' + [i]).each(function() {
-				$(this).height(maxHeight);
-			});
-		}
-	});
-	
-	// Add class to '.m-group's that have a programme in them	
-	$('.m-group').children('article').parent().addClass('programme');
-
-	// If programme starts at 00 (on the hour) add relevant class
-	$('.programme').each(function() {
-		var minutes = $(this).find('.minutes').text();
-		if (minutes == 0) {
-			$(this).addClass('on-the-hour');
-		}
-	});
-	
-	// I flippin' did it! What a difference two weeks' holiday makes!!!!!!!! Get in!!!!!!! :)
-	// This finds out empty heights (used to align progs) and adds it to the previous prog, then iterates thru grid
-	$('.programme').siblings().nextUntil('.programme').each(function() {
-		var total = $(this).height();
-		//console.log(total);
-		var sum = total;
-			sum - total;
-		//console.log(sum);
-		$(this).prev('.programme').css('height', '+=' + sum);
-		$(this).remove();
-	});
-	
-	// Uses plugin to keep programme details in view when user scrolls down to see suceeding progs
-	// Will work awesomely once programme div can expand into available true prog space/height
-	$('.m-group').ready(function(){
-		var height = $(this).height();
-		if (height > 100) {
-			$('.front article dl').jScroll({speed : 200, top: 85});
-		}
-	});
-	
-	// Removes all those '.m-group' divs from the DOM that have no content/height
-	$('.m-group').each(function(){
-		var height = $(this).height();
-		if (height == 0) {
-			$(this).remove();
-		}
-	});
-	
-	/*
-		- PROG GENRES -- for adding to aid easier sight of films, news etc
-		- - - - - - - - - - - -
-		- Adds HTML that contains necessary classes for hooking into font-awesome fonts and CSS
-	*/
-	
-	$('.film').each(function() {
-		$(this).children().children('dt').prepend(' <i class="icon-film"></i>');
-	});
-	$('.sport').each(function() {
-		$(this).children().children('dt').prepend(' <i class="icon-trophy"></i>');
-	});
-	$('.news').each(function() {
-		$(this).children().children('dt').prepend(' <i class="icon-quote-right"></i>');
-	});
-	$('.quiz').each(function() {
-		$(this).children().children('dt').prepend(' <i class="icon-comment-alt"></i>');
-	});
-	$('.food').each(function() {
-		$(this).children().children('dt').prepend(' <i class="icon-food"></i>');
-	});	
-	
-};
+<?php $thisPage="Listings"; ?>
+<!DOCTYPE html>
+<html dir="ltr" lang="en-GB">
+	<head>
+		<title>box</title>
+		<?php include('includes/head.php'); ?>
+	</head>
+	<body>
+		<div class="wrapper">
+			<div class="section front">	
+				<header>
+					<ul>
+						<li><a href="" class="exposeBack topControl"><i class="icon-reorder"></i><span>Back</span></a></li>
+						<li><a href="" class="exposeCalendar"><span class="calday">Today</span><span class="calnumber">31</span></a></li>
+						<li><a href="" class="exposeColumns topControl"><i class="icon-columns"></i><span>Choose columns</span></a></li>
+					</ul>
+				</header>
+				<div class="container">
+					<div class="channels">
+						<ul>
+							<li class="channel">
+								<h1>BBC 1</h1>
+								<div id="bbc1" class="gutter"></div>
+							</li>
+							<li class="channel">
+								<h1>BBC 2</h1>
+								<div id="bbc2" class="gutter"></div>
+							</li>
+							<li class="channel">
+								<h1>BBC 3</h1>
+								<div id="bbc3" class="gutter"></div>
+							</li>
+							<li class="channel">
+								<h1>BBC 4</h1>
+								<div id="bbc4" class="gutter"></div>
+							</li>
+							<li class="channel">
+								<h1>CBBC</h1>
+								<div id="cbbc" class="gutter"></div>
+							</li>
+							<li class="channel">
+								<h1>CBeebies</h1>
+								<div id="cbeebies" class="gutter"></div>
+							</li>
+							<li class="channel">
+								<h1>BBC News</h1>
+								<div id="news" class="gutter"></div>
+							</li>
+							<li class="channel">
+								<h1>BBC Parliament</h1>
+								<div id="parliament" class="gutter"></div>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="calendar">
+					<!-- <img src="images/appfeatured-3.gif" alt="Temporary" /> -->
+					<table border="0" cellpadding="0" cellspacing="0">
+						<tr>
+							<th>Wed</th>
+							<th>Thu</th>
+							<th>Fri</th>
+							<th>Sat</th>
+							<th>Sun</th>
+							<th>Mon</th>
+							<th>Tue</th>
+						</tr>
+						<tr>
+							<td>
+								<a href="">
+									<span class="calnumber">29</span>
+								</a>	
+							</td>
+							<td class="">
+								<a href="">
+									<span class="calnumber">30</span>
+								</a>	
+							</td>
+							<td class="active today">
+								<a href="">
+									<span class="calnumber">31</span>
+								</a>	
+							</td>
+							<td>
+								<a href="">
+									<span class="calnumber">1</span>
+								</a>	
+							</td>
+							<td>
+								<a href="">
+									<span class="calnumber">2</span>
+								</a>	
+							</td>
+							<td>
+								<a href="">
+									<span class="calnumber">3</span>
+								</a>	
+							</td>
+							<td>
+								<a href="">
+									<span class="calnumber">4</span>
+								</a>	
+							</td>	
+						</tr>
+						<tr>
+							<td>
+								<a href="">
+									<span class="calnumber">5</span>
+								</a>	
+							</td>
+							<td>
+								<a href="">
+									<span class="calnumber">6</span>
+								</a>	
+							</td>
+							<td>
+								<a href="">
+									<span class="calnumber">7</span>
+								</a>	
+							</td>
+							<td class="inactive">
+								<a href="">
+									<span class="calnumber">8</span>
+								</a>	
+							</td>
+							<td class="inactive">
+								<a href="">
+									<span class="calnumber">9</span>
+								</a>	
+							</td>
+							<td class="inactive">
+								<a href="">
+									<span class="calnumber">10</span>
+								</a>	
+							</td>
+							<td class="inactive">
+								<a href="">
+									<span class="calnumber">11</span>
+								</a>	
+							</td>	
+						</tr>
+					</table>
+				</div>
+			</div>
+			<div class="section back">
+				<?php include('includes/controls.php'); ?>
+			</div>
+			<div class="overlay">
+				<div class="page">
+					<!--article class="film">
+						<dl>
+							<dt>Those Magnificent Men in Their Flying Machines <span class="year">(1965)</span></dt>
+							<dd class="p-time"><span class="hour">9</span>:<span class="minutes">00</span> &ndash; <span class="hour">10</span>:<span class="minutes">30</span></dd>
+							<dd class="p-channel">BBC 1</dd>
+							<dd class="p-details">
+								<ul>
+									<li class="replay"><a href="">Catch-up</a></li>
+									<li class="hd">HD</li>
+									<li class="widescreen">W</li>
+									<li class="subtitles">S</li>
+									<li class="repeat">R</li>
+									<li class="length">120 mins</li>
+								</ul>
+							</dd>
+						</dl>
+						<figure>
+							<img src="images/appfeatured-2.jpg" alt="Road" />
+							<figcaption>Philip Larkin had it right...</figcaption>
+						</figure>
+						<div class="col">
+							<p>A rare event for cult movie fans, BBC Four, that doyen of sophiscated European TV and film (yeah, right) brings this cinematic classic to the viewers of post-watershed crap TV.</p>
+							<p>Hey, I could write TV reviews for a living. A few superlatives here, a few choice words there. Easy. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+							<p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+						</div>
+					</article-->
+          <article class="film">
+							<dl style="width:90%">
+								<dt id="p-name"></dt>
+								<dd id="p-time"></dd>
+								<dd id="p-channel"></dd>
+								<dd id="p-details">
+									<ul>
+										<!--li class="hd">HD</li>
+										<li class="widescreen">W</li>
+										<li class="subtitles">S</li-->
+										<li class="repeat"></li>
+										<li class="replay"></li>
+										<li class="length"></li>
+									</ul>
+								</dd>
+							</dl>
+							<figure>
+								<img id="p-img" src="" alt="" />
+								<figcaption></figcaption>
+							</figure>
+							<div id="p-desc" class="col"></div>
+								
+						</article>
+					<a href="" class="closeOverlay">Close</a>
+				</div>
+			</div>
+		</div>
+		
+		<script type="text/javascript">
+			var carousel = new Carousel('.channels');
+			carousel.init();
+			
+			var a=$(".controls a");
+			for(var i=0;i<a.length;i++)
+			{
+			    a[i].onclick=function()
+			    {
+			        window.location=this.getAttribute("href");
+			        return false
+			    }
+			}
+		</script>
+	</body>
+</html>
